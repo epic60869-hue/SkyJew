10297e8434509677f1916c42c91a2093c0e63681
epic60869-hue <epic60869@gmail.com>
2026-09-23 20:35:40 +0100

    Fix calculator compilation

--- src/main/java/com/epic60869/skyjew/SkyJewCalculator.java
+++ src/main/java/com/epic60869/skyjew/SkyJewCalculator.java
@@ -24,7 +24,7 @@ public final class SkyJewCalculator {
         }
         return String.format(Locale.ROOT, "%,.10f", value)
             .replaceAll("0+$", "")
-            .replaceAll("\.$", "");
+            .replaceAll("\\.$", "");
     }
 
     private static final class Parser {
