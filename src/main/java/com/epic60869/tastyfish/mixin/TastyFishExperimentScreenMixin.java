afd17c273bf01516480ea2eb8877f3c489484d3c
epic60869-hue <epic60869@gmail.com>
2026-09-23 18:29:32 +0100

    Add experimentation helper screen mixin

---
+++ src/main/java/com/epic60869/tastyfish/mixin/TastyFishExperimentScreenMixin.java
@@ -0,0 +1,30 @@
+package com.epic60869.tastyfish.mixin;
+
+import com.epic60869.tastyfish.TastyFishExperimentHelper;
+import net.minecraft.client.gui.GuiGraphicsExtractor;
+import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.client.input.MouseButtonEvent;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(AbstractContainerScreen.class)
+public abstract class TastyFishExperimentScreenMixin {
+    @Inject(method = "extractRenderState", at = @At("TAIL"))
+    private void tastyfish$renderExperimentHelper(
+        GuiGraphicsExtractor graphics, int mouseX, int mouseY, float delta, org.spongepowered.asm.mixin.injection.callback.CallbackInfo ci
+    ) {
+        TastyFishExperimentHelper.render(graphics, (AbstractContainerScreen<?>) (Object) this);
+    }
+
+    @Inject(method = "mouseClicked", at = @At("HEAD"), cancellable = true)
+    private void tastyfish$preventExperimentMisclick(
+        MouseButtonEvent event, boolean doubleClick, CallbackInfoReturnable<Boolean> cir
+    ) {
+        AbstractContainerScreen<?> screen = (AbstractContainerScreen<?>) (Object) this;
+        if (TastyFishExperimentHelper.shouldBlockClick(screen, event.x(), event.y())) {
+            cir.setReturnValue(true);
+        }
+    }
+}
