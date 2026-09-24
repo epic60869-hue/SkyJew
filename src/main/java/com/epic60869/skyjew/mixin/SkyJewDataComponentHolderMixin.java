9c97c7e1bf62f18d024491ae58ec2e259b03b3ea
epic60869-hue <epic60869@gmail.com>
2026-09-24 20:35:56 +0100

    Render Skyblocker-style glint and item model overrides client-side

--- src/main/java/com/epic60869/skyjew/mixin/SkyJewDataComponentHolderMixin.java
+++ src/main/java/com/epic60869/skyjew/mixin/SkyJewDataComponentHolderMixin.java
@@ -5,7 +5,7 @@ import net.minecraft.core.component.DataComponentHolder;
 import net.minecraft.core.component.DataComponentType;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.equipment.trim.ArmorTrim;
+import net.minecraft.world.item.equipment.trim.ArmorTrim;\nimport net.minecraft.resources.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
