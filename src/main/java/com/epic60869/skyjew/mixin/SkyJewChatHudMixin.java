c1e3d81331b1d1abf5ec89ebf96d76bc6d0e97a3
epic60869-hue <epic60869@gmail.com>
2026-09-24 20:58:23 +0100

    Restore chat mixin Inject import

--- src/main/java/com/epic60869/skyjew/mixin/SkyJewChatHudMixin.java
+++ src/main/java/com/epic60869/skyjew/mixin/SkyJewChatHudMixin.java
@@ -14,6 +14,7 @@ import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import java.util.List;
 
